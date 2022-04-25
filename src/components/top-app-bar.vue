b3a159394f1efd67978478fdac935c4d3bbfea6a
john-mirage <chaosdynamix@posteo.net>
2022-04-26 01:29:52 +0200

    feat: add mobile table and filter for leaderboards

--- src/components/top-app-bar.vue
+++ src/components/top-app-bar.vue
@@ -1,86 +1,95 @@
 <script setup>
-import { ref, watch } from "vue";
-import { Popover, PopoverButton, PopoverPanel, Switch } from "@headlessui/vue";
-import { MenuIcon, SunIcon, MoonIcon } from "@heroicons/vue/solid";
+import {ref, watch} from "vue";
+import {Popover, PopoverButton, PopoverPanel, Switch} from "@headlessui/vue";
+import {MenuIcon, SunIcon, MoonIcon, XIcon} from "@heroicons/vue/solid";
 
 const enabled = ref(false);
 
 watch(enabled, async (newEnabled, oldEnabled) => {
-    if (!oldEnabled && newEnabled) {
-        document.documentElement.classList.add("dark");
-    } else if (oldEnabled && !newEnabled) {
-        document.documentElement.classList.remove("dark");
-    }
+  if (!oldEnabled && newEnabled) {
+    document.documentElement.classList.add("dark");
+  } else if (oldEnabled && !newEnabled) {
+    document.documentElement.classList.remove("dark");
+  }
 });
 </script>
 
 <template>
-    <header class="w-full h-16 bg-slate-100 dark:bg-slate-800 shadow-md">
-        <div class="container px-6 mx-auto flex flex-row items-center h-full">
-            <div class="flex-1 flex flex-row justify-start">
-                <Popover class="relative">
-                    <PopoverButton>
-                        <span class="flex w-12 h-12 rounded-full cursor-pointer">
-                            <MenuIcon class="h-6 w-6 m-auto text-slate-500 dark:text-slate-50" />
-                        </span>
-                    </PopoverButton>
-                    <transition
-                        enter-active-class="transition duration-200 ease-out"
-                        enter-from-class="translate-y-1 opacity-0"
-                        enter-to-class="translate-y-0 opacity-100"
-                        leave-active-class="transition duration-150 ease-in"
-                        leave-from-class="translate-y-0 opacity-100"
-                        leave-to-class="translate-y-1 opacity-0"
-                    >
-                        <PopoverPanel class="absolute z-10">
-                            <nav
-                                class="flex flex-col w-auto h-auto space-y-4 p-6 bg-slate-50 dark:bg-slate-700 rounded-2xl shadow-lg"
-                            >
-                                <router-link
-                                    class="text-sm font-semibold text-slate-500 dark:text-slate-300 uppercase tracking-wider transition-colors hover:text-slate-900 dark:hover:text-slate-50"
-                                    to="/"
-                                >Accueil</router-link>
-                                <router-link
-                                    class="text-sm font-semibold text-slate-500 dark:text-slate-300 uppercase tracking-wider transition-colors hover:text-slate-900 dark:hover:text-slate-50"
-                                    to="/leaderboards"
-                                >Classements</router-link>
-                                <router-link
-                                    class="text-sm font-semibold text-slate-500 dark:text-slate-300 uppercase tracking-wider transition-colors hover:text-slate-900 dark:hover:text-slate-50"
-                                    to="/teams"
-                                >Equipes</router-link>
-                                <router-link
-                                    class="text-sm font-semibold text-slate-500 dark:text-slate-300 uppercase tracking-wider transition-colors hover:text-slate-900 dark:hover:text-slate-50"
-                                    to="/live"
-                                >Live</router-link>
-                            </nav>
-                        </PopoverPanel>
-                    </transition>
-                </Popover>
-            </div>
-            <div class="flex-1 flex flex-row justify-center"></div>
-            <div class="flex-1 flex flex-row justify-end">
-                <div class="flex items-center">
-                    <SunIcon
-                        class="h-5 w-5"
-                        :class="!enabled ? 'text-yellow-600' : 'text-slate-400'"
-                    />
-                    <Switch
-                        v-model="enabled"
-                        :class="enabled ? 'bg-slate-600' : 'bg-slate-300'"
-                        class="relative inline-flex items-center h-6 rounded-full w-11 mx-2"
-                    >
-                        <span class="sr-only">Enable dark mode</span>
-                        <span
-                            :class="enabled ? 'translate-x-6' : 'translate-x-1'"
-                            class="inline-block w-4 h-4 transition duration-200 ease-in-out transform bg-emerald-500 dark:bg-emerald-400 rounded-full"
-                        />
-                    </Switch>
-                    <MoonIcon
-                        class="h-5 w-5"
-                        :class="enabled ? 'text-yellow-200' : 'text-slate-400'"
-                    />
-                </div>
-            </div>
+  <header class="w-full h-16 bg-slate-100 dark:bg-slate-800 shadow-md">
+    <div class="container px-6 mx-auto flex flex-row items-center h-full">
+      <div class="flex-1 flex flex-row justify-start">
+        <Popover class="relative" v-slot="{ open }">
+          <PopoverButton>
+            <span class="flex w-12 h-12 rounded-full cursor-pointer -ml-3">
+              <MenuIcon v-if="!open" class="h-6 w-6 m-auto text-slate-500 dark:text-slate-50"/>
+              <XIcon v-else class="h-6 w-6 m-auto text-slate-500 dark:text-slate-50" />
+            </span>
+          </PopoverButton>
+          <transition
+              enter-active-class="transition duration-200 ease-out"
+              enter-from-class="translate-y-1 opacity-0"
+              enter-to-class="translate-y-0 opacity-100"
+              leave-active-class="transition duration-150 ease-in"
+              leave-from-class="translate-y-0 opacity-100"
+              leave-to-class="translate-y-1 opacity-0"
+          >
+            <PopoverPanel class="absolute z-10" v-slot="{ close }">
+              <nav
+                  class="flex flex-col w-auto h-auto space-y-4 p-6 bg-slate-50 dark:bg-slate-700 rounded-2xl shadow-lg"
+              >
+                <router-link
+                    @click="close"
+                    class="text-sm font-semibold text-slate-500 dark:text-slate-300 uppercase tracking-wider transition-colors hover:text-slate-900 dark:hover:text-slate-50"
+                    to="/"
+                >Accueil
+                </router-link>
+                <router-link
+                    @click="close"
+                    class="text-sm font-semibold text-slate-500 dark:text-slate-300 uppercase tracking-wider transition-colors hover:text-slate-900 dark:hover:text-slate-50"
+                    to="/leaderboards"
+                >Classements
+                </router-link>
+                <router-link
+                    @click="close"
+                    class="text-sm font-semibold text-slate-500 dark:text-slate-300 uppercase tracking-wider transition-colors hover:text-slate-900 dark:hover:text-slate-50"
+                    to="/teams"
+                >Equipes
+                </router-link>
+                <router-link
+                    @click="close"
+                    class="text-sm font-semibold text-slate-500 dark:text-slate-300 uppercase tracking-wider transition-colors hover:text-slate-900 dark:hover:text-slate-50"
+                    to="/live"
+                >Live
+                </router-link>
+              </nav>
+            </PopoverPanel>
+          </transition>
+        </Popover>
+      </div>
+      <div class="flex-1 flex flex-row justify-center"></div>
+      <div class="flex-1 flex flex-row justify-end">
+        <div class="flex items-center">
+          <SunIcon
+              class="h-5 w-5"
+              :class="!enabled ? 'text-yellow-600' : 'text-slate-400'"
+          />
+          <Switch
+              v-model="enabled"
+              :class="enabled ? 'bg-slate-600' : 'bg-slate-300'"
+              class="relative inline-flex items-center h-6 rounded-full w-11 mx-2"
+          >
+            <span class="sr-only">Enable dark mode</span>
+            <span
+                :class="enabled ? 'translate-x-6' : 'translate-x-1'"
+                class="inline-block w-4 h-4 transition duration-200 ease-in-out transform bg-emerald-500 dark:bg-emerald-400 rounded-full"
+            />
+          </Switch>
+          <MoonIcon
+              class="h-5 w-5"
+              :class="enabled ? 'text-yellow-200' : 'text-slate-400'"
+          />
         </div>
-    </header>
+      </div>
+    </div>
+  </header>
 </template>
--- src/pages/leaderboards.vue
+++ src/pages/leaderboards.vue
@@ -1,8 +1,120 @@
 <script setup>
+import {ref} from 'vue'
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOptions,
+  ListboxOption,
+} from '@headlessui/vue'
+import {CheckIcon, SelectorIcon} from '@heroicons/vue/solid'
+
+const filter = [
+  {name: 'Classement général'},
+  {name: 'La petite foulée (Q1)'},
+  {name: 'Remise en selle (Q2)'},
+  {name: 'En toute adherace (Q3)'},
+]
+const selectedPerson = ref(filter[0])
 </script>
 
 <template>
-    <main class="container mx-auto p-6">
-        <h1 class="text-2xl text-slate-900 dark:text-slate-50">Les classements</h1>
-    </main>
+  <main class="container mx-auto p-6">
+    <h1 class="text-2xl text-slate-900 dark:text-slate-50 mb-6">Les classements</h1>
+
+    <Listbox v-model="selectedPerson">
+      <div class="relative mt-1 mb-6">
+        <ListboxButton class="relative w-full py-3 pl-3 pr-10 text-left text-slate-50 bg-slate-800 rounded shadow-md cursor-pointer focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
+          <span class="block truncate">{{ selectedPerson.name }}</span>
+          <span class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+            <SelectorIcon class="w-5 h-5 text-gray-400" aria-hidden="true"/>
+          </span>
+        </ListboxButton>
+        <transition
+            leave-active-class="transition duration-100 ease-in"
+            leave-from-class="opacity-100"
+            leave-to-class="opacity-0"
+        >
+          <ListboxOptions class="absolute w-full py-1 mt-2 overflow-auto text-base bg-slate-700 rounded shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            <ListboxOption
+                v-slot="{ active, selected }"
+                v-for="view in filter"
+                :key="view.name"
+                :value="view"
+                as="template"
+            >
+              <li
+                  :class="[
+                  active ? 'text-slate-50 bg-slate-600' : 'text-slate-50',
+                  'cursor-default select-none relative py-2 pl-10 pr-4',
+                ]"
+              >
+                <span
+                    :class="[
+                    selected ? 'font-medium' : 'font-normal',
+                    'block truncate',
+                  ]"
+                >{{ view.name }}</span
+                >
+                <span
+                    v-if="selected"
+                    class="absolute inset-y-0 left-0 flex items-center pl-3 text-emerald-400"
+                >
+                  <CheckIcon class="w-5 h-5" aria-hidden="true"/>
+                </span>
+              </li>
+            </ListboxOption>
+          </ListboxOptions>
+        </transition>
+      </div>
+    </Listbox>
+
+    <table class="w-full">
+      <thead class="hidden">
+      <tr>
+        <th>Position</th>
+        <th>Equipe</th>
+        <th>Participant</th>
+        <th>Points</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr class="block mb-4 p-4 rounded bg-slate-800">
+        <td data-head="Position"
+            class="block text-slate-50 font-medium before:content-[attr(data-head)] before:text-slate-400 before:inline-block before:w-32">
+          1
+        </td>
+        <td data-head="Equipe"
+            class="block text-slate-50 font-medium before:content-[attr(data-head)] before:text-slate-400 before:inline-block before:w-32">
+          Rasta RoRocket
+        </td>
+        <td data-head="Participant"
+            class="block text-slate-50 font-medium before:content-[attr(data-head)] before:text-slate-400 before:inline-block before:w-32">
+          Jamie Ross
+        </td>
+        <td data-head="Points"
+            class="block text-slate-50 font-medium before:content-[attr(data-head)] before:text-slate-400 before:inline-block before:w-32">
+          293
+        </td>
+      </tr>
+      <tr class="block mb-4 p-4 rounded bg-slate-800">
+        <td data-head="Position"
+            class="block text-slate-50 font-medium before:content-[attr(data-head)] before:text-slate-400 before:inline-block before:w-32">
+          2
+        </td>
+        <td data-head="Equipe"
+            class="block text-slate-50 font-medium before:content-[attr(data-head)] before:text-slate-400 before:inline-block before:w-32">
+          Rasta RoRocket
+        </td>
+        <td data-head="Participant"
+            class="block text-slate-50 font-medium before:content-[attr(data-head)] before:text-slate-400 before:inline-block before:w-32">
+          Jamie Ross
+        </td>
+        <td data-head="Points"
+            class="block text-slate-50 font-medium before:content-[attr(data-head)] before:text-slate-400 before:inline-block before:w-32">
+          280
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </main>
 </template>
